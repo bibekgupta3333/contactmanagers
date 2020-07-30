47cf09480616509109b46d17500f7995746b771d
bibekgupta3333 <bibekgupta3333@gmail.com>
2020-07-31 00:43:12 +0545

    Updates

---
+++ precache-manifest.9312377af0dc76b549984a7322ed2591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03005e14d816b2ab5cfe748b2fee6e8",
+    "url": "/contactmanagers/index.html"
+  },
+  {
+    "revision": "1085a992942a89aee6f8",
+    "url": "/contactmanagers/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1085a992942a89aee6f8",
+    "url": "/contactmanagers/static/js/2.00bf2508.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanagers/static/js/2.00bf2508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2d7c49d721693821d7",
+    "url": "/contactmanagers/static/js/main.f30c5fef.chunk.js"
+  },
+  {
+    "revision": "7ce34a89d7d0047fa82f",
+    "url": "/contactmanagers/static/js/runtime-main.c0133bd3.js"
+  }
+]);
